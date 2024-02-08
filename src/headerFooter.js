f1eb406ac3ec73b55d6ac657dcbe81e2cf8fef5a
ThuYasWe <s10222373@connect.np.edu.sg>
2024-02-09 07:11:17 +0800

    Make changes to UI

--- src/headerFooter.js
+++ src/headerFooter.js
@@ -51,7 +51,6 @@ const loadHeaderFooter = (() => {
                     <h3>EcoToy</h3>
                     <ul>
                         <li><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
                         <li><a href="#">Blog</a></li>
                         <li><a href="#">Contact</a></li>
                     </ul>
